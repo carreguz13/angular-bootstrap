08a090fccbee77499265ba796e1ced184ef34108
carlos <carreguz13@gmail.com>
2023-01-02 16:07:47 -0700

    navbar done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeContentComponent } from './components/home-content/home-content.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
- 
+    NavbarComponent,
+    HomeContentComponent,
 
   ],
   imports: [
